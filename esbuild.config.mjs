e521c1164414473715e9ce080ae51f170d622a31
wesswart77 <wesswart77@users.noreply.github.com>
2026-05-24 22:06:57 +0200

    v1.0.1: fix description, remove builtin-modules, fix conflicting IDs

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,12 +1,12 @@
 import esbuild from "esbuild";
 import process from "process";
-import builtins from "builtin-modules";
 
+const builtins = ["path","os","fs","child_process","crypto","events","http","https","net","stream","url","util","zlib","assert","buffer","constants","domain","module","punycode","querystring","readline","string_decoder","sys","timers","tty","v8","vm","worker_threads"];
 const prod = process.argv[2] === "production";
 const context = await esbuild.context({
   entryPoints: ["src/main.ts"],
   bundle: true,
-  external: ["obsidian", "electron", "@codemirror/autocomplete", "@codemirror/collab", "@codemirror/commands", "@codemirror/language", "@codemirror/lint", "@codemirror/search", "@codemirror/state", "@codemirror/view", "@lezer/common", "@lezer/highlight", "@lezer/lr", ...builtins],
+  external: ["obsidian","electron","@codemirror/autocomplete","@codemirror/collab","@codemirror/commands","@codemirror/language","@codemirror/lint","@codemirror/search","@codemirror/state","@codemirror/view","@lezer/common","@lezer/highlight","@lezer/lr",...builtins],
   format: "cjs",
   target: "es2018",
   logLevel: "info",
